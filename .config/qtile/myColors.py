600de0db4d39ba44b71fef92b38d55c20cb54cac
Jorge Rubio <211607848+jorgeorc@users.noreply.github.com>
2025-05-16 18:22:30 -0600

    Add qtile config

---
+++ .config/qtile/myColors.py
@@ -0,0 +1,55 @@
+RosePine = {
+    "dark": ["#1f1f28"],  # Fondo oscuro
+    "light": ["#e0def4"],  # Color base claro
+    "gray": ["#6e6a86"],  # Gris (tono oscuro neutro)
+    "red": "#eb6f92",  # Rojo
+    "green": "#31748f",  # Verde
+    "yellow": "#e0af68",  # Amarillo
+    "blue": "#9ccfd8",  # Azul
+    "magenta": "#c4a7e7",  # Magenta
+    "cyan": "#9ccfd8",  # Cyan
+    "white": "#c8c093",  # Blanco
+    "orange": "#f6c177",  # Naranja
+}
+
+OneDark = {
+    "dark": ["#0d1117"],  # Fondo oscuro
+    "light": ["#abb2bf"],  # Color base claro
+    "gray": ["#5c6370"],  # Gris (tono oscuro neutro)
+    "red": "#ef596f",  # Rojo
+    "green": "#89ca78",  # Verde
+    "yellow": "#e5c07b",  # Amarillo
+    "blue": "#61afef",  # Azul
+    "magenta": "#d55fde",  # Magenta
+    "cyan": "#2bbac5",  # Cyan
+    "white": "#dedede",  # Blanco
+    "orange": "#d19a66",  # Naranja
+}
+
+Gruvbox = {
+    "dark": ["#0d0e0f"],  # Fondo oscuro
+    "light": ["#ebdbb2"],  # Color base claro
+    "gray": ["#504945"],  # Gris (tono oscuro neutro)
+    "red": "#cc241d",  # Rojo
+    "green": "#689d6a",  # Verde
+    "yellow": "#fabd2f",  # Amarillo
+    "blue": "#458588",  # Azul
+    "magenta": "#b16286",  # Magenta
+    "cyan": "#7fa2ac",  # Cyan
+    "white": "#dedede",  # Blanco
+    "orange": "#d65d0e",  # Naranja
+}
+
+Monokai = {
+        "dark": "#101014",
+        "light": "#C0CAF5",
+        "gray": "#363b54",
+        "red": "#f7768e",
+        "green": "#9ece6a",
+        "yellow": "#e0af68",
+        "blue": "#7aa2f7",
+        "magenta": "#9d7cd8",
+        "cyan": "#0DB9D7",
+        "white": "#787c99",
+        "orange": "#d65d0e",
+}
